e8a631010c3d9a49bd931c748d47e25f6185dea0
Armando Gutiérrez <gutierrezjarr@gmail.com>
2018-06-22 23:08:27 -0800

    visualizacion de Drink favorito en top-level

--- app/src/main/java/com/armando/starbuzz2/TopLevelActivity.java
+++ app/src/main/java/com/armando/starbuzz2/TopLevelActivity.java
@@ -7,14 +7,67 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.view.View;
 
+/*Para la lista de favoritos*/
+import android.database.Cursor;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.SimpleCursorAdapter;
+import android.widget.CursorAdapter;
+import android.widget.Toast;
 
 public class TopLevelActivity extends Activity {
+
+    private SQLiteDatabase db;
+    private Cursor favoritesCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        setupOptionsListView();
+        setupFavoriteListView();
+    }
 
+    private void setupFavoriteListView() {
+        ListView listFavorites = (ListView) findViewById(R.id.list_favorites);
+
+        try {
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            favoritesCursor = db.query(StarbuzzDatabaseHelper.TABLE_DRINK,
+                    new String[]{"_id", "NAME"},
+                    "FAVORITE = 1",
+                    null,null,null,null);
+            CursorAdapter favoriteAdapter = new SimpleCursorAdapter(TopLevelActivity.this,
+                    android.R.layout.simple_list_item_1,
+                    favoritesCursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1},0);
+            listFavorites.setAdapter(favoriteAdapter);
+        }catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this,"Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        listFavorites.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(TopLevelActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int)id);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        favoritesCursor.close();
+        db.close();
+    }
+
+    private void setupOptionsListView(){
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
